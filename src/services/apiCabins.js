a64415dec596ce611deb2c5514099bbf32186085
karl ducta <karlducta022@gmail.com>
2024-03-07 00:22:30 +0800

    ------------- Supabase ---------------

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("An error occurred while fetching cabins");
+  }
+
+  return data;
+}
