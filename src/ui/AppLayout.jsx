7d546027a6a4195ffb113d32dc399d262a9fbabb
karl ducta <karlducta022@gmail.com>
2024-03-08 02:49:21 +0800

    Displaying Toast -- Notifications

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -15,6 +15,14 @@ const Main = styled.main`
   padding: 4rem 4.8rem 6.4rem;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
@@ -22,7 +30,9 @@ function AppLayout() {
       <Sidebar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
