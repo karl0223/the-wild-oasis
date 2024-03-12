bafe878713c6bba26b0ab8859f1360b2e3cb3753
karl ducta <karlducta022@gmail.com>
2024-03-12 23:48:32 +0800

    API-Side Filtering: Filtering Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : {
+          field: "status",
+          value: filterValue,
+          method: "eq",
+        };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter], // filter is used as part of the query key to ensure that the query is re-fetch when the filter changes
+    queryFn: () => getBookings({ filter }),
   });
 
   return {
