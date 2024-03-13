90c197a2ae87a5da469c04d1e3bc170f7583bfe6
karl ducta <karlducta022@gmail.com>
2024-03-13 16:56:30 +0800

    Prefetching with React Query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -35,6 +37,21 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page], // filter and sortBy are used as part of the query key to ensure that the query are re-fetch when the filter changes
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1], // Pre-fetch the next page of bookings
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }), // Pre-fetch the next page of bookings
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1], // Pre-fetch the prev page of bookings
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }), // Pre-fetch the prev page of bookings
+    });
 
   return {
     isLoading,
